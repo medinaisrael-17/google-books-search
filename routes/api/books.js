8a6a9add72a9e990c54d9a943f8fdcd733174d03
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-05-02 15:03:17 -0500

    routes

--- routes/api/books.js
+++ routes/api/books.js
@@ -15,6 +15,10 @@ const booksController = require("../../controllers/booksController");
 
 router.route("/")
     .get(booksController.findAll)
+    .post(booksController.create)
+
+router.route("/:id")
+    .delete(booksController.remove);
 
 
 module.exports = router;
